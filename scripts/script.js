1bb8825f26c3fd5c8371eca7871e8f876c87f1ec
C L <leungc22@gmail.com>
2025-03-12 16:39:42 -0400

    part 3 and part 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
 const toggleButtons = document.querySelectorAll('.project-toggle');
+const contactForm = document.getElementById('contactForm');
+
+
+// Event Listeners
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nameValue = document.getElementById('name').value;
+    const emailValue = document.getElementById('email').value;
+    const subjectValue = document.getElementById('subject').value;
+    const messageValue = document.getElementById('message').value;
+
+    if (nameValue === '' || emailValue === '' || subjectValue === '' || messageValue === '') {
+        alert('Please fill out all fields.');
+      } else {
+        alert('Form submitted');
+      }
+  });
 
 toggleButtons.forEach(button => {
   button.addEventListener('click', function() {
@@ -6,4 +24,12 @@ toggleButtons.forEach(button => {
     
     details.classList.toggle('show');
   });
+});
+
+//
+var typed = new Typed('#typed', {
+  strings: ['Web Developer', 'Project Manager', 'Freelancer'],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true
 });
