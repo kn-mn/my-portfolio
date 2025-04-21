5ef032cd625b52f0042c9e59e0a122bf8510effe
CL <leungc22@gmail.com>
2025-04-21 19:18:13 -0400

    fixed dark mode and styling

--- scripts/script.js
+++ scripts/script.js
@@ -69,10 +69,10 @@ var typed = new Typed('#typed', {
 // Navigation Bar Hamburger 
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('header nav');
+menuToggle.addEventListener('click', ()=> nav.classList.toggle('open'));
 
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
+document.querySelectorAll('header nav a')
+  .forEach(link => link.addEventListener('click', ()=> nav.classList.remove('open')));
 
 document.querySelectorAll('header nav a').forEach(link => {
   link.addEventListener('click', () => {
